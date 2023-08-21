1a760d74ef6b7012ab8cd792e93bd541a1895db5
Aryan Godara <aryangodara03@gmail.com>
2023-08-21 15:04:36 +0530

    Initial commit
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,29 @@
-let () = print_endline "Hello, World!"
+let main () =
+  (*TODO: Initialize Portmidi *)
+  print_endline "Calling Midi.init ()\n";
+  Midi.init ();
+
+  (*TODO: List Devices  *)
+  print_endline "Calling Midi.List_devices.list_devices ()\n";
+  List_devices.list_devices () |> ignore;
+  ()
+
+let () =
+  main ();
+  (* Tcp_server.run_server () *)
+  let () = Logs.set_reporter (Logs.format_reporter ()) in
+  let () = Logs.set_level (Some Logs.Info) in
+
+  (* Server *)
+  let server_socket = Udp_server.create_socket () in
+  let client_socket = Udp_client.create_socket () in
+
+  let thre =
+    Lwt.pick
+      [
+        Udp_server.create_server server_socket;
+        Udp_client.create_client client_socket;
+      ]
+  in
+
+  Lwt_main.run thre
