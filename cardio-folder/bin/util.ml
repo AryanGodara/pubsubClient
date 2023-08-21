417c0bb507de42720f2ee3d84a7642f9a8562f2b
Aryan Godara <aryangodara03@gmail.com>
2023-08-21 23:03:01 +0530

    Initial commit
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- cardio-folder/bin/util.ml
+++
@@ -1,6 +0,0 @@
-(* this file is imported from Patrick Ferris's work at https://github.com/patricoferris/runtime-events-demo/ *)
-let child_alive child_pid () =
-  match Unix.waitpid [ Unix.WNOHANG ] child_pid with
-  | 0, _ -> true
-  | p, _ when p = child_pid -> false
-  | _, _ -> assert false
