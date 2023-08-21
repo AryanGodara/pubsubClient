6abbda740955c5c05db376f6d958cf78f07664ea
Aryan Godara <aryangodara03@gmail.com>
2023-08-21 23:32:30 +0530

    Initial commit
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

---
+++ bin/main.ml
@@ -0,0 +1,43 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Daniel C. Bünzli. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
+open Cmdliner
+
+let cmds = [ List_devices.cmd; Simple_engine.cmd; Stat_engine.cmd ]
+
+(* Command line interface *)
+
+let doc = "Auditively feel the work of the runtime "
+let sdocs = Manpage.s_common_options
+let man = [ `S Manpage.s_description; `P "Listen to the OCaml runtime" ]
+
+let main =
+  Cmd.group
+    (Cmd.info "cardio-crumble" ~version:"%%VERSION%%" ~doc ~sdocs ~man)
+    cmds
+
+let main () = Stdlib.exit @@ Cmd.eval' main
+let () = 
+let () = Logs.set_reporter (Logs.format_repor ter ()) in
+  let () = Logs.set_level (Some Logs.Info) in
+  UdpClient.init ();
+  main ()
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Daniel C. Bünzli
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
